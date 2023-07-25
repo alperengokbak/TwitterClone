b5d2a2c9bee6fdd29b9089986a7b1ea938296078
Alperen Gökbak <swalperen3008@gmail.com>
2023-07-25 16:28:50 +0300

    Configured database Connection

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
+
 const app = express();
+const port = 3000;
+
+app.use(express.json);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-  console.log("It's listening 5000.port.");
-});
+app.listen(port, () => console.log(`app listening on port ${port}`));
+
+/* 
+psql -U postgres --> to login PostgreSQL
+\l --> Listing the databases
+\c --> Connecting the database
+\! clear --> To clean the terminal page
+\dt --> To list the tables
+*/
