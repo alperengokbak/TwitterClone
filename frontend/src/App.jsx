734a6ab8111c56d85baa332d6160ef3843f1bd39
Alperen Gökbak <swalperen3008@gmail.com>
2023-07-28 10:34:33 +0300

    Sidebar Component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        {/* Sidebar */}
+        {/* Main Content */}
+        {/* Widgets */}
+      </div>
+    </>
+  );
+}
+
+export default App;
