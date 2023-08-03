5d0cd6cd6d60ede7e1b6cd0da3a9161d49161151
Alperen Gökbak <swalperen3008@gmail.com>
2023-08-03 13:50:05 +0300

    Configured the window size

--- frontend/src/SidebarOption.jsx
+++ frontend/src/SidebarOption.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Grid } from "@mui/material";
 
 function SidebarOptions({ text, Icon }) {
+  const [showText, setShowText] = useState(window.innerWidth >= 768);
+  useEffect(() => {
+    const handleResize = () => {
+      setShowText(window.innerWidth >= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <Grid container={true} padding="8px" spacing={1}>
       <Button>
         <Grid item color="#000000">
           <Icon />
         </Grid>
-        <Grid
-          item
-          fontWeight="600"
-          fontSize="16px"
-          marginRight="20px"
-          color="#000000"
-        >
-          {text}
-        </Grid>
+        {showText && (
+          <Grid
+            item
+            fontWeight="600"
+            fontSize="16px"
+            marginRight="20px"
+            color="#000000"
+          >
+            {text}
+          </Grid>
+        )}
       </Button>
     </Grid>
   );
